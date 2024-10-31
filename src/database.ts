ab5dfed7f4b8a9188f19b8d75ad7c3cd82582d76
ANDERSON CASSIO DE OLIVEIRA JUNIOR <anderson.junior@BRSPPM-GHHQ49C992.local>
2024-10-31 19:27:12 -0300

    Production Config

--- src/database.ts
+++ src/database.ts
@@ -4,11 +4,13 @@ import { env } from './env'
 // Migrations -> historico de todas as mudanças feitas no banco de dados
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
-  useNullAsDefault: true,
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
